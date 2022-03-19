ab77b2a5a3797437cc8ee3539827823fdddf5105
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2022-03-19 22:36:54 +0100

    servicio usuario

--- src/main/java/com/lucreciaguisasola/portafolio/servicios/IUsuarioServicio.java
+++ src/main/java/com/lucreciaguisasola/portafolio/servicios/IUsuarioServicio.java
@@ -1,15 +1,14 @@
 
 package com.lucreciaguisasola.portafolio.servicios;
 
-import com.lucreciaguisasola.portafolio.modelo.Roles;
 import com.lucreciaguisasola.portafolio.modelo.Usuario;
 import java.util.List;
 
 public interface IUsuarioServicio {
     
-    public void persistirUsuario(String usuario, String password, Roles rol) throws Exception;
-    public List<Usuario> listarUsuarios();
-    public Usuario buscarUsuario(Long id);
-    public void borrarUsuario(Long id);
+    public void persistirUsuario(String usuario, String password) throws Exception;
+    public List<Usuario> listarUsuarios() throws Exception;
+    public Usuario buscarUsuario(Long id) throws Exception;
+    public void borrarUsuario(Long id) throws Exception;
 
 }
--- src/main/java/com/lucreciaguisasola/portafolio/servicios/UsuarioServicio.java
+++ src/main/java/com/lucreciaguisasola/portafolio/servicios/UsuarioServicio.java
@@ -2,7 +2,6 @@
 package com.lucreciaguisasola.portafolio.servicios;
 
 import com.lucreciaguisasola.portafolio.excepciones.MiExcepcion;
-import com.lucreciaguisasola.portafolio.modelo.Roles;
 import static com.lucreciaguisasola.portafolio.modelo.Roles.INVITADO;
 import com.lucreciaguisasola.portafolio.modelo.Usuario;
 import com.lucreciaguisasola.portafolio.repositorios.UsuarioRepositorio;
@@ -16,17 +15,30 @@ public class UsuarioServicio implements IUsuarioServicio {
     @Autowired
     private UsuarioRepositorio usuarioRepo;
 
-    private boolean usuarioValido(String usuario){
-        
-        return (usuario.length()>=8);
+    private boolean usuarioValido(String usuario) throws MiExcepcion{
+        boolean valido = false;
+        if (usuarioRepo.buscarPorUsuario(usuario) != null){
+            throw new MiExcepcion("El usuario ya se encuentra registrado");
+        } else if (usuario.length()>=8){
+            throw new MiExcepcion("El usuario debe tener como mínimo 8 caracteres");
+        } else {
+            valido = true;
+        }
+        return valido;
     }
     
-    private boolean passwordValida(String password){
-        return (password.length()>=8);
+    private boolean passwordValida(String password) throws MiExcepcion{
+        boolean valida = false;
+        if (password.length()>=8){
+            throw new MiExcepcion("La contraseña debe tener como mínimo 8 caracteres");
+        } else {
+            valida = true;
+        }
+        return valida;
     }
             
     @Override
-    public void persistirUsuario(String usuario, String password, Roles rol) throws Exception{
+    public void persistirUsuario(String usuario, String password) throws Exception{
         try {
             Usuario u = new Usuario();
             if(usuarioValido(usuario)){
@@ -43,25 +55,38 @@ public class UsuarioServicio implements IUsuarioServicio {
             usuarioRepo.save(u);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            throw new MiExcepcion("Error al registrar el usuario");
+            throw new MiExcepcion("Error al guardar el usuario");
         }
     }
+    
     @Override
-    public List<Usuario> listarUsuarios() {
+    public List<Usuario> listarUsuarios() throws Exception{
         try {
-            
+            return usuarioRepo.findAll();
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new MiExcepcion("No se encontró ningún usuario");
         }
     }
 
     @Override
-    public Usuario buscarUsuario(Long id) {
-        
+    public Usuario buscarUsuario(Long id) throws MiExcepcion {
+        try {
+            return usuarioRepo.findById(id).orElse(null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new MiExcepcion("No se encontró el usuario");
+        }
     }
 
     @Override
-    public void borrarUsuario(Long id) {
-        
+    public void borrarUsuario(Long id) throws MiExcepcion {
+        try {
+            usuarioRepo.deleteById(id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new MiExcepcion("Error al eliminar usuario");
+        }
     }
 
     
