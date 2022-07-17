9f9ca33460b35d7be005cc3512ea64538c475731
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2022-07-17 10:33:29 +1000

    "bean en main"

--- src/main/java/com/lucreciaguisasola/portafolio/PortafolioApplication.java
+++ src/main/java/com/lucreciaguisasola/portafolio/PortafolioApplication.java
@@ -2,6 +2,9 @@ package com.lucreciaguisasola.portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortafolioApplication {
@@ -9,5 +12,16 @@ public class PortafolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioApplication.class, args);
 	}
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer(){
+                @Override
+                public void addCorsMappings(CorsRegistry registry){
+                    registry.addMapping("/*").allowedOrigins("https://portafolio-es.web.app/").allowedHeaders("").allowedMethods("*");
+                    
+                }
+            };
+        }
 
 }
