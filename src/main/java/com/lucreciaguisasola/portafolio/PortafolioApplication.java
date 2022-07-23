092e4147e671af98b01ce1c43762173dec70281b
Lucrecia Guisasola <lucreciaguisasola@hotmail.com>
2022-07-24 09:15:44 +1000

    cambio main

--- src/main/java/com/lucreciaguisasola/portafolio/PortafolioApplication.java
+++ src/main/java/com/lucreciaguisasola/portafolio/PortafolioApplication.java
@@ -19,7 +19,7 @@ public class PortafolioApplication {
                 @Override
                 public void addCorsMappings(CorsRegistry registry){
                     registry.addMapping("/*").
-                            allowedOrigins("https://portafolio-es.web.app/portafolio").
+                            allowedOrigins("https://portafolio-es.web.app/").
                             allowedHeaders("").allowedMethods("*");
                     
                 }
